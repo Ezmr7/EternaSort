ed46b4ca7df480277cc168a686872f9bce4014e6
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2024-01-29 15:19:03 -0600

    Began webpack coniguration, enabled source-mapping for debugging, setup loaders for scss and image files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+import path from 'path'
+import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const webpack = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle[contenthash].js',
+    clean: true,
+      assetModuleFilename: '[name][ext]'
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8080,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
+}
+
+export default webpack;
