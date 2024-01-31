4fc1e6a7993c302ab2d4baa18f1deb2846831199
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2024-01-30 19:19:41 -0600

    Started basic React file structure with TypeScript

---
+++ src/containers/CardContainer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CardContainer = () => {
+  return (
+    <div className='CardContainer'>
+      <h1>This is the card container</h1>
+    </div>
+  )
+}
+
+export default CardContainer;
