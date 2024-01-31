1a849604a38a47d184bf7cbf10c0bd0ceafcf5dd
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2024-01-30 19:21:01 -0600

    Created Node/Express.js server for backend, also utliziing TypeScript

---
+++ src/server/server.ts
@@ -0,0 +1,37 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**
+ * handle requests for static files
+ */
+app.use(express.static(path.resolve(__dirname, '../client')));
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log('--------------------------',
+  `Server listening on port: ${PORT}...`,
+   '--------------------------');
+});
+
+export default app
