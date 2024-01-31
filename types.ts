bf76e9085c5fa31e4ba504ec5a861d3bcb8e6f0e
Ezekiel Mohr <85984467+Ezmr7@users.noreply.github.com>
2024-01-30 19:13:56 -0600

    added types file, with interface for Card objects

---
+++ types.ts
@@ -0,0 +1,15 @@
+export interface CardInterface {
+  Name: string;
+  CardType: string;
+  MonsterType: string;
+  Type: string;
+  Attribute: string;
+  Property: string;
+  Level: number;
+  ATK: number;
+  DEF: number;
+  Lore: string;
+  Sets: string[];
+  Decks: string[];
+  ImageName: string;
+}
